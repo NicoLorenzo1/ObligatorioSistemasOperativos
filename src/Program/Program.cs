235a4e0aafa13064b5595fe708377cc1ef99175d
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-06 22:31:55 -0300

    Timer implementado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,7 +8,7 @@ namespace Program
         {
             Proceso proceso1 = new Proceso("proceso1", 4, 60);
             Proceso proceso2 = new Proceso("proceso2", 3, 100);
-            //Proceso proceso3 = new Proceso("proceso3", 6, 50);
+            Proceso proceso3 = new Proceso("proceso3", 6, 50);
             //Proceso proceso4 = new Proceso("proceso4", 2, 120);
 
 
@@ -16,6 +16,7 @@ namespace Program
         //Planificador.LogicaPlanificador();
         Cpu cpu = new Cpu();
         cpu.FinishTimeGame();
+        //cpu.OrderByPriority();
 
         }
     }

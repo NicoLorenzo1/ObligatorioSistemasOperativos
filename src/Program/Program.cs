eb5ec45398a2960b16f6f867b24ad606f6d1088f
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-09 01:05:32 -0300

    Tiempo de E/S añadido

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,18 +6,19 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            Proceso proceso1 = new Proceso("proceso1", 8, 100, 0, 0);
-            Proceso proceso2 = new Proceso("proceso2", 6, 100, 2, 3);
-            Proceso proceso3 = new Proceso("proceso3", 10, 100, 0, 0);
+            //Proceso proceso1 = new Proceso("proceso1", 8, 100, 0, 0);
+            //Proceso proceso2 = new Proceso("proceso2", 6, 100, 2, 3);   //preguntar si se suma cpu con es
+            //Proceso proceso3 = new Proceso("proceso3", 10, 100, 0, 0);
             //Proceso proceso4 = new Proceso("proceso4", 2, 120);
 
 
        //Console.WriteLine(Planificador.queue.Count);
         //Planificador.LogicaPlanificador();
         Cpu cpu = new Cpu();
-        cpu.FinishTimeGame();
-        cpu.BloquearProceso();
+        cpu.TimerCounter();
         //cpu.OrderByPriority();
+
+        LectorArchivo.Read();
 
         }
     }

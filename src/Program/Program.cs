9efda4ae8d5bd2f7df6474a31547988f54d4c133
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-13 20:59:24 -0300

    Implementamos leer archivo y e/s

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,15 +6,13 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            //Proceso proceso1 = new Proceso("proceso1", 8, 100, 0, 0);
-            //Proceso proceso2 = new Proceso("proceso2", 6, 100, 2, 3);   //preguntar si se suma cpu con es
+            //Proceso proceso1 = new Proceso("proceso1", 4, 100, 3, 1);
+            //Proceso proceso2 = new Proceso("proceso2", 6, 100, 3, 1);   //preguntar si se suma cpu con es
             //Proceso proceso3 = new Proceso("proceso3", 10, 100, 0, 0);
             //Proceso proceso4 = new Proceso("proceso4", 2, 120);
 
             LectorArchivo.Read();
 
-            //Console.WriteLine(Planificador.queue.Count);
-            //Planificador.LogicaPlanificador();
             Cpu cpu = new Cpu();
             cpu.TimerCounter();
             //cpu.OrderByPriority();

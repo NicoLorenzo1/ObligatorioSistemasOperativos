be3e0a85f3a1c4c5af9eef505400c03d27d99923
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-06-18 04:21:59 -0300

    Sin terminar

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,14 +10,14 @@ namespace Program
             // Proceso proceso2 = new Proceso("proceso2", 6, 100, 3, 4, false);
             // Proceso proceso3 = new Proceso("proceso3", 8, 50, 2, 3, true);
             // Proceso proceso4 = new Proceso("proceso4", 2, 120);
-
+            //Planificador.queue.Add(proceso1);
             LectorArchivo.Read();
 
             Planificador.OrderByPriority();
             Cpu cpu = new Cpu();
 
             //false muestra listas y true muestra paso a paso 
-            cpu.TimerCounter(true);
+            cpu.TimerCounter(false);
 
 
         }
